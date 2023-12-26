acbb1efff494cb0e5b6058ef03494d87525f8726
ThinkStu <91378285+Bistutu@users.noreply.github.com>
2023-12-26 14:32:21 +0800

    opti

--- misc/manuals/manual_test.go
+++ misc/manuals/manual_test.go
@@ -22,7 +22,7 @@ import (
 var (
 	ctx  = context.Background()
 	link = utils.GetHostByString(
-		"https://openai.com/",
+		"https://star-history.com/",
 		//"https://platform.openai.com/apps",
 		//"https://chat.openai.com/",
 		//"https://help.openai.com/en/articles/8554397-creating-a-gpt",
