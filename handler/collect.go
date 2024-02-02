556d31e9efac58ee0215db702fd20547e52423d9
ThinkStu <91378285+Bistutu@users.noreply.github.com>
2024-02-02 20:47:27 +0800

    删除多余函数

--- handler/collect.go
+++
@@ -1,7 +0,0 @@
-package handler
-
-import "github.com/gin-gonic/gin"
-
-func CollectHandler(c *gin.Context) {
-	// 24.1.11 应该由前端插件实现
-}
--- handler/router.go
+++ handler/router.go
@@ -17,8 +17,6 @@ func NewRouter() *gin.Engine {
 	// 绑定路由
 	engine.POST("/preread", PreReadHandler) // 预读接口
 	engine.POST("/read", ReadHandler)       // 按 host 全文读取接口
-	engine.POST("/trans", TransHandler)     // 翻译接口
-	engine.POST("/collect", CollectHandler) // 前端文本收集接口（暂未实现）
 
 	return engine
 }
