538441bdfa0366ff4de788ed53ce4ddec17c4d36
hewigovens <360470+hewigovens@users.noreply.github.com>
2025-08-12 13:34:34 +0900

    Refactor context menu ID to use constant
    Replaced hardcoded context menu ID with CONTEXT_MENU_IDS.TRANSLATE_FULL_PAGE constant for better maintainability. Added CONTEXT_MENU_IDS to constant.ts.

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,6 +1,7 @@
 import {_service} from "@/entrypoints/service/_service";
 import {config} from "@/entrypoints/utils/config";
 import {reportTranslationCount} from "@/entrypoints/utils/influx-reporter";
+import {CONTEXT_MENU_IDS} from "@/entrypoints/utils/constant";
 
 export default defineBackground({
     persistent: {
@@ -9,14 +10,14 @@ export default defineBackground({
     main() {
         // 创建右键菜单项
         browser.contextMenus.create({
-            id: 'fluent-read-translate-full-page',
+            id: CONTEXT_MENU_IDS.TRANSLATE_FULL_PAGE,
             title: 'FluentRead - 全文翻译',
             contexts: ['page', 'selection'],
         });
 
         // 监听右键菜单点击事件
         browser.contextMenus.onClicked.addListener((info, tab) => {
-            if (info.menuItemId === 'fluent-read-translate-full-page' && tab?.id) {
+            if (info.menuItemId === CONTEXT_MENU_IDS.TRANSLATE_FULL_PAGE && tab?.id) {
                 // 发送消息到内容脚本触发全文翻译
                 browser.tabs.sendMessage(tab.id, {
                     type: 'contextMenuTranslate',
